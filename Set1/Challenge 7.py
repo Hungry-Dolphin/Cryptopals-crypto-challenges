0d57cc3b7dc68e2e9715e3b0065727709ed81f13
Hungry-Dolphin <spoekie99@gmail.com>
2023-02-22 22:35:56 +0100

    Copied the read file lines from challenge 6, forgot to replace the 6 with a 7. Took me WAY to long to find that

---
+++ Set1/Challenge 7.py
@@ -0,0 +1,19 @@
+import os
+import codecs
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import binascii
+
+def main():
+    message = open(os.path.join(os.getcwd(), 'Input/Set1/Input 7.txt'), 'rb').read()
+    message = codecs.decode(message, 'base64')
+    key = codecs.encode('YELLOW SUBMARINE', 'utf-8')
+
+    print(decrypt_AES(message, key))
+
+def decrypt_AES(message, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return codecs.decode(cipher.decrypt(message), 'utf-8')
+
+if __name__ == '__main__':
+    main()
